d98ceb58fd9700b2ee862e475b1e59a24bec3413
Kathryn Lund <kathryn.d.lund@gmail.com>
2023-05-22 13:20:19 +0200

    Add additional condition to keep iter_cholqr from running forever

--- main/muscles/iter_cholqr.m
+++ main/muscles/iter_cholqr.m
@@ -21,7 +21,7 @@ if isnan(loss_ortho)
     fprintf('%s failed to converge\n', mfilename);
     return
 end
-while abs(loss_ortho - sqrt(s)*eps) > eps
+while (abs(loss_ortho - sqrt(s)*eps) > eps) && (iter < 2*s)
     iter = iter + 1;
     A = Q' * Q;
     [R2, flag] = chol(A);
@@ -32,7 +32,7 @@ while abs(loss_ortho - sqrt(s)*eps) > eps
     Q = Q/R2;
     R = R2*R;
     loss_ortho = norm(Q' * Q - I, 'fro'); % sync point!
-    if loss_ortho == 1
+    if loss_ortho >= 1
         % then there's probably a zero column and the difference will never
         % be less than 1
         Q = NaN(m,s);
