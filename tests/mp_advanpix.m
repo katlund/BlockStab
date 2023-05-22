bb760c069ea2e7ef799a7cac77d4c2305ae50758
Kathryn Lund <kathryn.d.lund@gmail.com>
2023-05-22 15:50:43 +0200

    Correct config_file in mp_advanpix.m

--- tests/mp_advanpix.m
+++ tests/mp_advanpix.m
@@ -3,5 +3,5 @@ mat_type = 'glued';
 options.num_rows = 1000;
 options.num_partitions = 120;
 options.block_size = 8;
-config_file = 'mp.json';
+config_file = 'mp_advanpix.json';
 RunKappaPlot(mat_type, options, config_file);
