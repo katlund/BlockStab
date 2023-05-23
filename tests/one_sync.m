558f12aa3454a0f97ca85804cc86180cb6e894c5
Kathryn Lund <kathryn.d.lund@gmail.com>
2023-05-23 15:11:47 +0200

    Expand one_sync.m

--- tests/one_sync.m
+++ tests/one_sync.m
@@ -1,7 +1,16 @@
 % All one-sync methods in comparison with BMGS and HouseQR
-mat_type = 'monomial';
+config_file = 'one_sync.json';
+
+mat_type = {'glued', 'laeuchli', 'default'};
 options.num_rows = 1000;
+for i = 1:3
+    RunKappaPlot(mat_type{i}, options, config_file);
+    close all;
+end
+
+mat_type = 'monomial';
 options.num_partitions = 120;
 options.block_size = 8;
 config_file = 'one_sync.json';
 RunKappaPlot(mat_type, options, config_file);
+close all;
