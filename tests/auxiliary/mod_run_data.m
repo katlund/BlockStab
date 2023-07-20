ce63c79789bea5c647828261261391c705789fc6
Kathryn Lund <kathryn.d.lund@gmail.com>
2023-07-20 17:10:30 +0200

    Debug roadmap_f and mod_run_data

---
+++ tests/auxiliary/mod_run_data.m
@@ -0,0 +1,13 @@
+function run_data = mod_run_data(run_data, ind)
+% run_data = MOD_RUN_DATA(run_data, ind) is a subroutine for extracting a
+% subset of run_data entries according to integer indices ind for
+% replotting with GEN_PLOT.
+
+%%
+run_data.loss_ortho = run_data.loss_ortho(:,ind);
+run_data.rel_res = run_data.rel_res(:,ind);
+run_data.rel_chol_res = run_data.rel_chol_res(:,ind);
+run_data.lgd = run_data.lgd(ind);
+run_data.alg_cmap = run_data.alg_cmap(ind,:);
+run_data.symb = run_data.symb(ind);
+end
--- tests/roadmap_f.m
+++ tests/roadmap_f.m
@@ -8,52 +8,29 @@ options.block_size = 8;
 config_file = 'roadmap_f.json';
 run_data = RunKappaPlot(mat_type, options, config_file);
 
-%% File strings
-dir_str = sprintf('results/%s/%s_m%d_p%d_s%d', ...
-    mfilename, mat_type, options.num_rows, options.num_partitions, options.block_size);
-
 %% Stepwise plots for paper
+options.save_eps = true; options.save_fig = false;
+
 % BCGS(HouseQR, CholQR) up to BCGSI+(HouseQR, CholQR)
-new_dir_str = sprintf('%s/1_bcgs_to_iro', dir_str);
-new_run_data = run_data;
+new_dir_str = sprintf('%s/1_bcgs_to_iro', run_data.dir_str);
+mkdir(new_dir_str);
 ind = 1:4;
-new_run_data.loss_ortho = run_data.loss_ortho(ind);
-new_run_data.rel_res = run_data.rel_res(ind);
-new_run_data.rel_chol_res = run_data.rel_chol_res(ind);
-new_run_data.lgd = run_data.lgd(ind);
-new_run_data.alg_cmap = run_data.alg_cmap(ind,:);
-new_run_data.symb = run_data.symb(ind);
-gen_plots(run_data, new_dir_str);
+gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
 
 % BCGSI+(HouseQR, CholQR) up to BCGSI+F(HouseQR, CholQR)
-new_dir_str = sprintf('%s/2_iro_to_iro_f', dir_str);
+new_dir_str = sprintf('%s/2_iro_to_iro_f', run_data.dir_str);
+mkdir(new_dir_str);
 ind = 3:6;
-new_run_data.loss_ortho = run_data.loss_ortho(ind);
-new_run_data.rel_res = run_data.rel_res(ind);
-new_run_data.rel_chol_res = run_data.rel_chol_res(ind);
-new_run_data.lgd = run_data.lgd(ind);
-new_run_data.alg_cmap = run_data.alg_cmap(ind,:);
-new_run_data.symb = run_data.symb(ind);
-gen_plots(run_data, new_dir_str);
+gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
 
 % BCGSI+F(HouseQR, CholQR) up to BCGSI+F-3S(HouseQR, CholQR)
-new_dir_str = sprintf('%s/3_f4s_to_f3s', dir_str);
+new_dir_str = sprintf('%s/3_f4s_to_f3s', run_data.dir_str);
+mkdir(new_dir_str);
 ind = 5:8;
-new_run_data.loss_ortho = run_data.loss_ortho(ind);
-new_run_data.rel_res = run_data.rel_res(ind);
-new_run_data.rel_chol_res = run_data.rel_chol_res(ind);
-new_run_data.lgd = run_data.lgd(ind);
-new_run_data.alg_cmap = run_data.alg_cmap(ind,:);
-new_run_data.symb = run_data.symb(ind);
-gen_plots(run_data, new_dir_str);
+gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
 
 % BCGSI+F-3S(CholQR) up to BCGSI+F-1S(CholQR)
-new_dir_str = sprintf('%s/4_f3s_to_f1s', dir_str);
+new_dir_str = sprintf('%s/4_f3s_to_f1s', run_data.dir_str);
+mkdir(new_dir_str);
 ind = 8:10;
-new_run_data.loss_ortho = run_data.loss_ortho(ind);
-new_run_data.rel_res = run_data.rel_res(ind);
-new_run_data.rel_chol_res = run_data.rel_chol_res(ind);
-new_run_data.lgd = run_data.lgd(ind);
-new_run_data.alg_cmap = run_data.alg_cmap(ind,:);
-new_run_data.symb = run_data.symb(ind);
-gen_plots(run_data, new_dir_str);
+gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
