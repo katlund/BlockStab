3c82b78c3d316ddb5cfdc7dc001063322e62b800
Kathryn Lund <kathryn.d.lund@gmail.com>
2023-07-20 16:14:27 +0200

    Extract gen_plots

--- tests/roadmap_f.m
+++ tests/roadmap_f.m
@@ -1,8 +1,11 @@
-% Reproduce plot from "roadmap" discussion, focusing on methods with
-% reorthogonalized first vector
+%% ROADMAP_F
+% Reproduce plot from "roadmap" discussion, focusing on methods with O(eps)
+% first vector
 mat_type = 'monomial';
 options.num_rows = 1000;
 options.num_partitions = 120;
 options.block_size = 8;
 config_file = 'roadmap_f.json';
 RunKappaPlot(mat_type, options, config_file);
+
+%% Reconstruct plots stepwise for paper
