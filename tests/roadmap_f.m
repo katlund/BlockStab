9584d279ea5ab735bef8c8896435a1e68a967475
Kathryn Lund <kathryn.d.lund@gmail.com>
2023-07-21 10:18:41 +0200

    Increase font size in gen_plots

--- tests/roadmap_f.m
+++ tests/roadmap_f.m
@@ -7,6 +7,7 @@ options.num_partitions = 120;
 options.block_size = 8;
 config_file = 'roadmap_f.json';
 run_data = RunKappaPlot(mat_type, options, config_file);
+close all;
 
 %% Stepwise plots for paper
 options.save_eps = true; options.save_fig = false;
@@ -16,26 +17,31 @@ new_dir_str = sprintf('%s/1_bcgs_to_iro', run_data.dir_str);
 mkdir(new_dir_str);
 ind = 1:4;
 gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
+close all;
 
 % BCGSI+(HouseQR, CholQR) up to BCGSI+F(HouseQR, CholQR)
 new_dir_str = sprintf('%s/2_iro_to_iro_f', run_data.dir_str);
 mkdir(new_dir_str);
 ind = 3:6;
 gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
+close all;
 
 % BCGSI+F(HouseQR, CholQR) up to BCGSI+F-3S(HouseQR, CholQR)
 new_dir_str = sprintf('%s/3_f4s_to_f3s', run_data.dir_str);
 mkdir(new_dir_str);
 ind = 5:8;
 gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
+close all;
 
 % BCGSI+F-3S(CholQR) up to BCGSI+F-1S(CholQR)
 new_dir_str = sprintf('%s/4_f3s_to_f1s', run_data.dir_str);
 mkdir(new_dir_str);
 ind = 8:10;
 gen_plots(mod_run_data(run_data, ind), options, new_dir_str);
+close all;
 
 %% Better behaved example
 mat_type = 'default';
 config_file = 'roadmap_f.json';
 run_data = RunKappaPlot(mat_type, [], config_file);
+close all;
