9754bbe73a002132fad39915f8717885e56204c8
edoktay <edaoktay95@gmail.com>
2024-02-14 18:17:05 +0100

    add more json files and experiments

--- tests/bcgs_pip_reortho_paper.m
+++ tests/bcgs_pip_reortho_paper.m
@@ -14,8 +14,15 @@ config_file = 'reorth_vs_mp.json';
 RunKappaPlot(mat_type, [], config_file);
 
 mat_type = 'monomial';
+options.num_rows = 2000;
+options.num_partitions = 120;
+options.block_size = 10;
+config_file = 'reorth.json';
+RunKappaPlot(mat_type, options, config_file);
+
+mat_type = 'laeuchli';
 options.num_rows = 1000;
 options.num_partitions = 120;
 options.block_size = 8;
-config_file = 'pip_vs_reorth.json';
+config_file = 'reorth_vs_mp_house.json';
 RunKappaPlot(mat_type, options, config_file);
