ecce87eb8b018f449348cea62f470994d8c94294
edoktay <edaoktay95@gmail.com>
2024-03-26 11:34:38 +0100

    update paper figures

--- tests/bcgs_pip_reortho_paper.m
+++ tests/bcgs_pip_reortho_paper.m
@@ -21,11 +21,11 @@ config_file = 'reorth_vs_mp.json';
 RunKappaPlot(mat_type, [], config_file);
 %%
 mat_type = 'piled';
-options.num_rows = 100;%1000;
-options.num_partitions = 8;%10;
-options.block_size = 2;%10;
+options.num_rows = 1000;
+options.num_partitions = 10;
+options.block_size = 10;
 config_file = 'pip_pipi_mp_only_2_prec.json';
-run_data = RunKappaPlot_mp(mat_type, options, config_file,'double');
+run_data = RunKappaPlot(mat_type, options, config_file);
 
 
 
